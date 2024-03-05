67dfd1ebd7573de75ebcb123c682399bfb7fdcb3
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-03-05 10:07:44 +0100

    some sort of filtering for index

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -5,29 +5,6 @@
     <input type="checkbox" id="vegan" value="vegan" v-model="checkedNames" />
     <label for="vegan">Vegan</label>
 
-    <input
-      type="checkbox"
-      id="vegetarian"
-      value="vegetarian"
-      v-model="checkedNames"
-    />
-    <label for="vegetarian">Vegetarian</label>
-
-    <input
-      type="checkbox"
-      id="glutenFree"
-      value="glutenFree"
-      v-model="checkedNames"
-    />
-    <label for="glutenFree">Gluten Free</label>
-
-    <input
-      type="checkbox"
-      id="dairyFree"
-      value="dairyFree"
-      v-model="checkedNames"
-    />
-    <label for="dairyFree">Dairy Free</label>
     <div class="grid grid-cols-2 md:grid-cols-3 gap-3 sm:mx-2">
       <RecipeCard
         v-for="recipe in recipesSort"
@@ -40,20 +17,21 @@
 
 <script setup>
   const id = ref('');
-  const filterNamesTest = ref({
-    vegan: null,
-    vegetarian: null,
-    glutenFree: null,
-    dairyFree: null,
-  });
+  const vegan = ref(null);
   const checkedNamesTest = ref([]);
 
-  const { data: recipesSort } = useLazyFetch('http://localhost:4000/recipes/', {
-    query: {
-      origin: id,
-      vegan: filterNamesTest.value.vegan,
+  const { data: recipesSort, pending } = useLazyFetch(
+    'http://localhost:4000/recipes/',
+    {
+      query: {
+        origin: id,
+        vegan: vegan,
+      },
     },
-  });
+    {
+      immediate: false,
+    }
+  );
 
   const checkedNames = computed({
     get: () => {
@@ -62,27 +40,20 @@
     set: (value) => {
       checkedNamesTest.value = value;
       const updateFilterNames = (value) => {
-        const filterNames = {
-          vegan: value.includes('vegan') ? '1' : '0',
-          vegetarian: value.includes('vegetarian') ? '1' : '0',
-          glutenFree: value.includes('glutenFree') ? '1' : '0',
-          dairyFree: value.includes('dairyFree') ? '1' : '0',
+        vegan.value = value.includes('vegan') ? '1' : '0';
+        return {
+          vegan: vegan.value,
         };
-
-        filterNamesTest.value = { ...filterNamesTest.value, ...filterNames };
-        console.log('filterNamesTest.value', filterNamesTest.value);
       };
 
       // Usage:
       if (value.length === 0) {
-        filterNamesTest.value = {
+        vegan.value = null;
+        return {
           vegan: null,
-          vegetarian: null,
-          glutenFree: null,
-          dairyFree: null,
         };
       } else {
-        updateFilterNames(value);
+        return updateFilterNames(value);
       }
     },
   });
